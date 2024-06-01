2ba0bae8f843fe18a27d2c910e7a2a50c24c5cd6
ELEANOR ABDELMUTAL <EABDELMUTAL87608@uat.edu>
2024-06-01 19:59:27 -0400

    Test again

--- thedigitalizationprojectproposal/Form1.cs
+++ thedigitalizationprojectproposal/Form1.cs
@@ -147,7 +147,7 @@ namespace thedigitalizationprojectproposal
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+           //
         }
 
         private void communicateArticleDescriptionButton_Click(object sender, EventArgs e)
